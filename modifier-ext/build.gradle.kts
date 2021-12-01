6fc014c6d1b8117fdc5196044672043c4bf2509d
Margarita <margo.himera@yandex.ru>
2021-12-01 19:16:34 +0300

    Surf init

---
+++ modifier-ext/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    `maven-publish`
+    id("com.jfrog.artifactory")
+}
+
+// dependencies versions
+val composeVersion: String by project
+
+// lib info
+version = "0.0.10"
+group = "ru.surfstudio.compose"
+
+publishing {
+    publications {
+        register("aar", MavenPublication::class) {
+            groupId = group.toString()
+            artifactId = project.name
+            artifact("$buildDir/outputs/aar/modifier-ext-$version-release.aar")
+        }
+    }
+}
+
+artifactory {
+    setContextUrl("https://artifactory.surfstudio.ru/artifactory")
+    publish {
+        repository {
+            setRepoKey("libs-release-local")
+            setUsername(System.getenv("surf_maven_username"))
+            setPassword(System.getenv("surf_maven_password"))
+        }
+        defaults {
+            publications("aar")
+            setPublishArtifacts(true)
+        }
+    }
+}
+
+android {
+
+    compileSdk = 30
+
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 31
+        setProperty("archivesBaseName", "modifier-ext-$version")
+    }
+}
+
+dependencies {
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+}
--- modifier-ext/src/main/kotlin/com/keygenqt/modifier/SpaceSize.kt
+++ modifier-ext/src/main/kotlin/com/keygenqt/modifier/SpaceSize.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Vitaliy Zarubin
+ * Copyright 2021 Surf LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package com.keygenqt.modifier
+ 
+package ru.surfstudio.compose.modifier.ext
 
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
